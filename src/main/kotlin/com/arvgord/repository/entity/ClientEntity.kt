74c25dc00a9630e5d8ed73c705b6a74307ed106c
Artem Gordienko <arvgord@gmail.com>
2023-01-13 14:31:45 +0700

    feat: implement test

---
+++ src/main/kotlin/com/arvgord/repository/entity/ClientEntity.kt
@@ -0,0 +1,29 @@
+package com.arvgord.repository.entity
+
+import jakarta.persistence.CascadeType
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.NamedAttributeNode
+import jakarta.persistence.NamedEntityGraph
+import jakarta.persistence.OneToMany
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "CLIENT")
+@NamedEntityGraph(
+    name = "ClientEntityTest",
+    attributeNodes = [NamedAttributeNode("accounts")]
+)
+class ClientEntity(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    var id: Long? = null,
+
+    @OneToMany(mappedBy = "client", cascade = [CascadeType.PERSIST])
+    var accounts: Set<AccountEntity> = mutableSetOf()
+)
---
+++ src/test/kotlin/IssueTest.kt
@@ -0,0 +1,98 @@
+import com.arvgord.repository.entity.AccountEntity
+import com.arvgord.repository.entity.ClientEntity
+import jakarta.persistence.EntityManager
+import jakarta.persistence.Persistence
+import org.junit.jupiter.api.Test
+import org.testcontainers.containers.MySQLContainer
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+import java.math.BigDecimal
+import java.util.*
+import kotlin.random.Random
+import kotlin.test.assertEquals
+
+@Testcontainers
+internal class IssueTest {
+
+    @Container
+    private val postgresqlContainer = PostgreSQLContainer("postgres:15.1")
+        .withInitScript("postgres.sql")
+
+    @Container
+    private val mySQLContainer = MySQLContainer("mysql:8.0.31")
+        .withInitScript("mysql.sql")
+
+    @Test
+    fun `test hibernate 6 get incorrect results with PostgreSQL`() {
+        postgresqlContainer.start()
+        val props = Properties()
+        props.setProperty("hibernate.connection.url", postgresqlContainer.jdbcUrl)
+        props.setProperty("hibernate.connection.driver_class", "org.postgresql.Driver")
+        props.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect")
+        val emf = Persistence.createEntityManagerFactory("test", props)
+        val em = emf.createEntityManager()
+        createClientsAndAccounts(em)
+        val graph = em.createEntityGraph("ClientEntityTest")
+        val count = em.createQuery("select count(c) from ClientEntity c", Long::class.java).singleResult
+        val firstPage = em.createQuery("select c from ClientEntity c", ClientEntity::class.java)
+            .setFirstResult(0)
+            .setMaxResults(2)
+            .setHint("jakarta.persistence.loadgraph", graph)
+            .resultList
+        val secondPage = em.createQuery("select c from ClientEntity c", ClientEntity::class.java)
+            .setFirstResult(2)
+            .setMaxResults(2)
+            .setHint("jakarta.persistence.loadgraph", graph)
+            .resultList
+        assertEquals(3, count)
+        assertEquals(2, firstPage.size)
+        assertEquals(1, secondPage.size)
+    }
+
+    @Test
+    fun `test hibernate 6 get incorrect results with MySQL`() {
+        mySQLContainer.start()
+        val props = Properties()
+        props.setProperty("hibernate.connection.url", mySQLContainer.jdbcUrl)
+        props.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver")
+        props.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
+        val emf = Persistence.createEntityManagerFactory("test", props)
+        val em = emf.createEntityManager()
+        createClientsAndAccounts(em)
+        val graph = em.createEntityGraph("ClientEntityTest")
+        val count = em.createQuery("select count(c) from ClientEntity c", Long::class.java).singleResult
+        val firstPage = em.createQuery("select c from ClientEntity c", ClientEntity::class.java)
+            .setFirstResult(0)
+            .setMaxResults(2)
+            .setHint("jakarta.persistence.loadgraph", graph)
+            .resultList
+        val secondPage = em.createQuery("select c from ClientEntity c", ClientEntity::class.java)
+            .setFirstResult(2)
+            .setMaxResults(2)
+            .setHint("jakarta.persistence.loadgraph", graph)
+            .resultList
+        assertEquals(3, count)
+        assertEquals(2, firstPage.size)
+        assertEquals(1, secondPage.size)
+    }
+
+    private fun createClientsAndAccounts(em: EntityManager) {
+        em.transaction.begin()
+        (1..3).forEach {
+            val account1 = AccountEntity(
+                amount = BigDecimal(Random.nextInt(0,100)),
+                number = Random.nextInt().toString()
+            )
+            val account2 = AccountEntity(
+                amount = BigDecimal(Random.nextInt(0,100)),
+                number = Random.nextInt().toString()
+            )
+            val client = ClientEntity(accounts = setOf(account1, account2))
+            account1.client = client
+            account2.client = client
+            em.persist(client)
+        }
+        em.transaction.commit()
+    }
+}
