bd655b8ef18e87674e3ee51cb624e2483ca7d609
Artem Gordienko <arvgord@gmail.com>
2023-02-27 00:10:54 +0700

    test: add additional tests with criteria api

--- src/test/kotlin/IssueTest.kt
+++ src/test/kotlin/IssueTest.kt
@@ -2,6 +2,7 @@ import com.arvgord.repository.entity.AccountEntity
 import com.arvgord.repository.entity.ClientEntity
 import jakarta.persistence.EntityManager
 import jakarta.persistence.Persistence
+import jakarta.persistence.criteria.JoinType
 import org.junit.jupiter.api.Test
 import org.testcontainers.containers.MySQLContainer
 import org.testcontainers.containers.PostgreSQLContainer
@@ -11,6 +12,7 @@ import java.math.BigDecimal
 import java.util.*
 import kotlin.random.Random
 import kotlin.test.assertEquals
+
 
 @Testcontainers
 internal class IssueTest {
@@ -26,13 +28,9 @@ internal class IssueTest {
     @Test
     fun `test hibernate 6 get incorrect results with PostgreSQL`() {
         postgresqlContainer.start()
-        val props = Properties()
-        props.setProperty("hibernate.connection.url", postgresqlContainer.jdbcUrl)
-        props.setProperty("hibernate.connection.driver_class", "org.postgresql.Driver")
-        props.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect")
-        val emf = Persistence.createEntityManagerFactory("test", props)
-        val em = emf.createEntityManager()
+        val em = createEntityManager(postgresqlContainer.jdbcUrl, "org.postgresql.Driver", "org.hibernate.dialect.PostgreSQLDialect")
         createClientsAndAccounts(em)
+
         val graph = em.createEntityGraph("ClientEntityTest")
         val count = em.createQuery("select count(c) from ClientEntity c", Long::class.java).singleResult
         val firstPage = em.createQuery("select c from ClientEntity c", ClientEntity::class.java)
@@ -45,6 +43,39 @@ internal class IssueTest {
             .setMaxResults(2)
             .setHint("jakarta.persistence.loadgraph", graph)
             .resultList
+
+        assertEquals(3, count)
+        assertEquals(2, firstPage.size)
+        assertEquals(1, secondPage.size)
+    }
+
+    @Test
+    fun `test hibernate 6 get incorrect results with PostgreSQL with criteria`() {
+        postgresqlContainer.start()
+        val em = createEntityManager(postgresqlContainer.jdbcUrl, "org.postgresql.Driver", "org.hibernate.dialect.PostgreSQLDialect")
+        createClientsAndAccounts(em)
+
+        val criteriaBuilder = em.criteriaBuilder
+
+        val criteriaQueryLong = criteriaBuilder.createQuery(Long::class.java)
+        val clientsCountRoot = criteriaQueryLong.from(ClientEntity::class.java)
+        val selectCount = criteriaQueryLong.select(criteriaBuilder.count(clientsCountRoot))
+        val count = em.createQuery(selectCount).singleResult
+
+        val criteriaQueryEntity = criteriaBuilder.createQuery(ClientEntity::class.java)
+        val clientsRoot = criteriaQueryEntity.from(ClientEntity::class.java)
+        val selectClients = criteriaQueryEntity.select(clientsRoot)
+        // Works with WARN: HHH90003004: firstResult/maxResults specified with collection fetch; applying in memory
+        clientsRoot.fetch<AccountEntity, ClientEntity>("accounts", JoinType.LEFT)
+        val firstPage = em.createQuery(selectClients)
+            .setFirstResult(0)
+            .setMaxResults(2)
+            .resultList
+        val secondPage = em.createQuery(selectClients)
+            .setFirstResult(2)
+            .setMaxResults(2)
+            .resultList
+
         assertEquals(3, count)
         assertEquals(2, firstPage.size)
         assertEquals(1, secondPage.size)
@@ -53,13 +84,9 @@ internal class IssueTest {
     @Test
     fun `test hibernate 6 get incorrect results with MySQL`() {
         mySQLContainer.start()
-        val props = Properties()
-        props.setProperty("hibernate.connection.url", mySQLContainer.jdbcUrl)
-        props.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver")
-        props.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
-        val emf = Persistence.createEntityManagerFactory("test", props)
-        val em = emf.createEntityManager()
+        val em = createEntityManager(mySQLContainer.jdbcUrl, "com.mysql.jdbc.Driver", "org.hibernate.dialect.MySQLDialect")
         createClientsAndAccounts(em)
+
         val graph = em.createEntityGraph("ClientEntityTest")
         val count = em.createQuery("select count(c) from ClientEntity c", Long::class.java).singleResult
         val firstPage = em.createQuery("select c from ClientEntity c", ClientEntity::class.java)
@@ -72,9 +99,50 @@ internal class IssueTest {
             .setMaxResults(2)
             .setHint("jakarta.persistence.loadgraph", graph)
             .resultList
+
         assertEquals(3, count)
         assertEquals(2, firstPage.size)
         assertEquals(1, secondPage.size)
+    }
+
+    @Test
+    fun `test hibernate 6 get incorrect results with MySQL with criteria`() {
+        mySQLContainer.start()
+        val em = createEntityManager(mySQLContainer.jdbcUrl, "com.mysql.jdbc.Driver", "org.hibernate.dialect.MySQLDialect")
+        createClientsAndAccounts(em)
+
+        val criteriaBuilder = em.criteriaBuilder
+
+        val criteriaQueryLong = criteriaBuilder.createQuery(Long::class.java)
+        val clientsCountRoot = criteriaQueryLong.from(ClientEntity::class.java)
+        val selectCount = criteriaQueryLong.select(criteriaBuilder.count(clientsCountRoot))
+        val count = em.createQuery(selectCount).singleResult
+
+        val criteriaQueryEntity = criteriaBuilder.createQuery(ClientEntity::class.java)
+        val clientsRoot = criteriaQueryEntity.from(ClientEntity::class.java)
+        val selectClients = criteriaQueryEntity.select(clientsRoot)
+        // Works with WARN: HHH90003004: firstResult/maxResults specified with collection fetch; applying in memory
+        clientsRoot.fetch<AccountEntity, ClientEntity>("accounts", JoinType.LEFT)
+        val firstPage = em.createQuery(selectClients)
+            .setFirstResult(0)
+            .setMaxResults(2)
+            .resultList
+        val secondPage = em.createQuery(selectClients)
+            .setFirstResult(2)
+            .setMaxResults(2)
+            .resultList
+
+        assertEquals(3, count)
+        assertEquals(2, firstPage.size)
+        assertEquals(1, secondPage.size)
+    }
+
+    private fun createEntityManager(url: String, driver: String, dialect: String): EntityManager {
+        val props = Properties()
+        props.setProperty("hibernate.connection.url", url)
+        props.setProperty("hibernate.connection.driver_class", driver)
+        props.setProperty("hibernate.dialect", dialect)
+        return Persistence.createEntityManagerFactory("test", props).createEntityManager()
     }
 
     private fun createClientsAndAccounts(em: EntityManager) {
